7bd6907741092b82404d68775e961a6ed7bb08f5
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2024-01-22 20:19:52 -0300

    feat: implements new script

---
+++ scripts/handlerCreateNewCardForm.js
@@ -0,0 +1,30 @@
+const addCardForm = document.querySelector(".popup-add-card");
+const title = document.querySelector(".profile__name");
+const image = document.querySelector(".profile__role");
+
+const inputTitle = document.querySelector("#input-location-name");
+const inputImage = document.querySelector("#input-image");
+const createButton = document.querySelector(".popup-add-card__create-button");
+
+const cards = document.querySelector(".elements").children;
+
+addCardForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newCard = cards[0].cloneNode(true);
+
+  newCard.childNodes[0].nextSibling.src = inputImage.value;
+  newCard.childNodes[0].nextSibling.alt = `Imagem do ${inputTitle.value}`;
+  newCard.childNodes[3].children[0].innerText = inputTitle.value;
+
+  const newCards = [newCard, ...cards];
+
+  document.querySelector(".elements").append(...newCards);
+
+  const button = document.querySelector(".elements__like-icon");
+
+  handleNewLikeButton(button);
+});
+
+createButton.addEventListener("click", () => {
+  addCardForm.classList.remove("popup-add-card_opened");
+});
