f8f3ac82cb3165a39a6b332cdcb5cb3c55234a6c
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2024-01-22 20:20:22 -0300

    feat: implements functions to handle like actions

--- scripts/likeButton.js
+++ scripts/likeButton.js
@@ -1,12 +1,23 @@
-const likeButtons = document.querySelectorAll(".elements__like-icon");
+const handleLikeButtons = () => {
+  const likeButtons = document.querySelectorAll(".elements__like-icon");
 
-likeButtons.forEach((button) => {
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (button.getAttribute("src") === "./images/like-button.png") {
+        return button.setAttribute("src", "./images/like-button-clicked.png");
+      }
+
+      return button.setAttribute("src", "./images/like-button.png");
+    });
+  });
+};
+
+const handleNewLikeButton = (button) => {
   button.addEventListener("click", (e) => {
-    console.log(button.getAttribute("src"));
     if (button.getAttribute("src") === "./images/like-button.png") {
       return button.setAttribute("src", "./images/like-button-clicked.png");
     }
 
     return button.setAttribute("src", "./images/like-button.png");
   });
-});
+};
